4644511baecab20de598f9a995d0c89bd33e1170
renatobcost <renatobcosta@alu.ufc.br>
2023-01-17 22:47:32 -0300

    updates

--- cypress/integration/home.spec.js
+++
@@ -1,9 +0,0 @@
-
-describe('home page', function(){
-    
-    it('app deve estar online', function(){
-        cy.visit('/')
-        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
-    })
-
-})
